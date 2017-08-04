02fb26fa9ef20dda40a13ab1d5519838cfa6c38c
Stephen Crane <sjc@immunant.com>
2017-08-04 10:41:41 -0700

    Initial structure

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+extern crate getopts;
+use getopts::Options;
+use std::env;
+
+fn print_usage (program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optopt("s", "server", "server address", "ADDRESS");
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+    let output = matches.opt_str("server").unwrap();
+    println!("Hello, world! {}", &output);
+}
