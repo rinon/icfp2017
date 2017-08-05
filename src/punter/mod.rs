1a04ea88a493a371c3554444475ce1235a4e931a
Andrei Homescu <ah@immunant.com>
2017-08-04 21:56:03 -0700

    Added basic data structures for State and River

--- src/punter/mod.rs
+++ src/punter/mod.rs
@@ -1,4 +1,20 @@
+use std::collections::HashSet;
+
 mod protocol;
+
+type PunterId = usize;
+type SiteId = usize;
+
+pub struct State {
+    rivers: Vec<River>,
+    mines: HashSet<SiteId>,
+}
+
+pub struct River {
+    source: SiteId,
+    target: SiteId,
+    owner: Option<PunterId>,
+}
 
 pub fn handshake() -> protocol::HandshakeP {
     protocol::HandshakeP {
